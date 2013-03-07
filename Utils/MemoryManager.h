7b4d4878074351c14ca70d890f2de06efe1bfbb9
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-03-07 18:38:00 +0100

    Major MemoryManager overhaul
    MemoryManager is a class that handles creation and deletion of heap
    allocated objects
    You can now choose (thanks to painful template metaprogramming)
    different container types for storage and deletion storage - this allows
    you to use sets and prevent accidental double deletions
    It is advised (in my opinion) to use MemoryManager whenever possible,
    since it shouldn't have any overhead over two containers of <T*>
    Currently supported containers are std::vector and
    google::dense_hash_set, but it's easy to write your own traits

--- Utils/MemoryManager.h
+++ Utils/MemoryManager.h
@@ -1,41 +1,49 @@
 #ifndef SSVS_UTILS_MEMORYMANAGER
 #define SSVS_UTILS_MEMORYMANAGER
 
-#include <vector>
 #include "Utils.h"
+#include "Traits/Traits.h"
 
 namespace ssvs
 {
 	namespace Utils
 	{
-		template<typename T> class MemoryManager
+		template<typename TItem, typename TContainer = std::vector<TItem*>, typename TDelContainer = std::vector<TItem*>> class MemoryManager
 		{
 			private:
-				std::vector<T*> items, itemsToDel;
+				typedef Traits::Container<TContainer, TItem*> ContainerTraits;
+				typedef Traits::Container<TDelContainer, TItem*> DelContainerTraits;
+
+				TContainer items;
+				TDelContainer itemsToDel;
 
 			public:
-				MemoryManager() = default;
+				MemoryManager() { ContainerTraits::init(items); DelContainerTraits::init(itemsToDel); }
 				~MemoryManager() { for(auto& i : items) delete i; }
 
-				template<typename... TArgs> T& create(TArgs&&... mArgs)
+				template<typename TItemType, typename... TArgs> TItemType& create(TArgs&&... mArgs)
 				{
-					T* result{new T(std::forward<TArgs>(mArgs)...)};
-					items.push_back(result);
-					return *result;
+					TItemType* result{new TItemType(std::forward<TArgs>(mArgs)...)};
+					ContainerTraits::add(items, static_cast<TItem*>(result)); return *result;
 				}
-				void del(T* mItem) { itemsToDel.push_back(mItem); }
+				template<typename... TArgs> TItem& create(TArgs&&... mArgs)
+				{
+					TItem* result{new TItem(std::forward<TArgs>(mArgs)...)};
+					ContainerTraits::add(items, result); return *result;
+				}
+				void del(TItem* mItem) { DelContainerTraits::add(itemsToDel, mItem); }
 				void cleanUp()
 				{
-					for(auto& i : itemsToDel) { eraseRemove(items, i); delete i; }
-					itemsToDel.clear();
+					for(auto& i : itemsToDel) { ContainerTraits::del(items, i); delete i; }
+					DelContainerTraits::clear(itemsToDel);
 				}
 				void clear()
 				{
 					for(auto& i : items) delete i;
-					items.clear();
-					itemsToDel.clear();
+					ContainerTraits::clear(items);
+					DelContainerTraits::clear(itemsToDel);
 				}
-				std::vector<T*>& getItems() { return items; }
+				TContainer& getItems() { return items; }
 		};
 	}
 }
---
+++ Utils/Traits/Traits.h
@@ -0,0 +1,44 @@
+#ifndef SSVS_UTILS_TRAITS
+#define SSVS_UTILS_TRAITS
+
+#include <vector>
+#include <sparsehash/dense_hash_set>
+
+namespace ssvs
+{
+	namespace Utils
+	{
+		namespace Traits
+		{
+			template<typename TContainer, typename TItem> struct Container
+			{
+				static void init(TContainer& mContainer);
+				static void clear(TContainer& mContainer);
+				static void add(TContainer& mContainer, const TItem& mItem);
+				static void del(TContainer& mContainer, const TItem& mItem);
+			};
+
+			template<typename TItem> struct Container<std::vector<TItem>, TItem>
+			{
+				typedef std::vector<TItem> TContainer;
+				
+				static void init(TContainer&) { }
+				static void clear(TContainer& mContainer) { mContainer.clear(); }
+				static void add(TContainer& mContainer, const TItem& mItem) { mContainer.push_back(mItem); }
+				static void del(TContainer& mContainer, const TItem& mItem) { eraseRemove(mContainer, mItem); }
+			};
+
+			template<typename TItem> struct Container<google::dense_hash_set<TItem>, TItem>
+			{
+				typedef google::dense_hash_set<TItem> TContainer;
+
+				static void init(TContainer& mContainer) { mContainer.set_empty_key(nullptr); }
+				static void clear(TContainer& mContainer) { mContainer.clear(); }
+				static void add(TContainer& mContainer, const TItem& mItem) { mContainer.insert(mItem); }
+				static void del(TContainer&, const TItem&) { }
+			};
+		}
+	}
+}
+
+#endif
