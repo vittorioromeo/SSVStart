211ccd93d05289f968161f730fb008530e3924e1
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-03-21 17:31:57 +0100

    Minor refactoring

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -10,10 +10,10 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
-#include "Utils/ThreadWrapper.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 #include <SSVUtils/SSVUtils.h>
+#include "Utils/ThreadWrapper.h"
 
 namespace ssvs
 {
@@ -21,15 +21,15 @@ namespace ssvs
 	{
 		// Angles
 		template<typename T> T getRadiansToPoint(const sf::Vector2<T>& mStart, const sf::Vector2<T>& mEnd) { return atan2(mEnd.y - mStart.y, mEnd.x - mStart.x); }
-		template<typename T> T getDegreesToPoint(const sf::Vector2<T>& mStart, const sf::Vector2<T>& mEnd) { return toDegrees(atan2(mEnd.y - mStart.y, mEnd.x - mStart.x)); }
+		template<typename T> T getDegreesToPoint(const sf::Vector2<T>& mStart, const sf::Vector2<T>& mEnd) { return toDegrees(getRadiansToPoint(mStart, mEnd)); }
 		
 		// Collision
 		bool isPointInPolygon(std::vector<sf::Vector2f*>, sf::Vector2f);
 
 		// Vectors
 		template<typename T> T getMagnitude(const sf::Vector2<T>& mVector) { return sqrt(mVector.x * mVector.x + mVector.y * mVector.y); }
-		template<typename T> sf::Vector2<T> getNormalized(const sf::Vector2<T>& mVector) { T m = getMagnitude(mVector); return {mVector.x / m, mVector.y / m}; }
-		template<typename T> T getRadians(const sf::Vector2<T>& mVector) { auto n = getNormalized(mVector); return atan2(n.y, n.x); }
+		template<typename T> sf::Vector2<T> getNormalized(const sf::Vector2<T>& mVector) { T m(getMagnitude(mVector)); return {mVector.x / m, mVector.y / m}; }
+		template<typename T> T getRadians(const sf::Vector2<T>& mVector) { auto n(getNormalized(mVector)); return atan2(n.y, n.x); }
 		template<typename T> T getDegrees(const sf::Vector2<T>& mVector) { return ssvu::toDegrees(getRadians(mVector)); }
 		template<typename T> sf::Vector2<T> getVectorFromRadians(T mRadians, T mMagnitude) { return sf::Vector2<T>(mMagnitude * cos(mRadians), mMagnitude * sin(mRadians)); }
 		template<typename T> sf::Vector2<T> getVectorFromDegrees(T mDegrees, T mMagnitude) { return getVectorFromRadians(ssvu::toRadians(mDegrees), mMagnitude); }
