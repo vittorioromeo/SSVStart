52b9ff3046ec22728495a425ce6c56b6d3111c3f
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-14 23:40:31 +0200

    Added "launched" bool to ThreadWrapper

--- include/SSVStart/Utils/ThreadWrapper.h
+++ include/SSVStart/Utils/ThreadWrapper.h
@@ -15,14 +15,16 @@ namespace ssvs
 		class ThreadWrapper
 		{
 			private:
+				bool launched{false};
 				bool finished{false};
 				std::function<void()> func;
 				sf::Thread thread;
 
 			public:
 				ThreadWrapper(std::function<void()> mFunction) : func{[&, mFunction]{ mFunction(); finished = true; }}, thread{func} { }
-				void launch()		{ thread.launch(); }
-				void terminate()	{ thread.terminate(); }
+				void launch()		{ thread.launch(); launched = true; }
+				void terminate()	{ thread.terminate(); finished = true; }
+				bool getLaunched()	{ return launched; }
 				bool getFinished()	{ return finished; }
 		};
 	}
