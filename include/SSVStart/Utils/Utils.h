ab13b0968fe61bc2d6730d8d27cd5fe249821e78
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-05 22:55:38 +0200

    Added more Vec utils

--- include/SSVStart/Utils/Utils.h
+++ include/SSVStart/Utils/Utils.h
@@ -27,24 +27,26 @@ namespace ssvs
 		// Collision
 		bool isPointInPolygon(const std::vector<Vec2f>& mVertices, Vec2f mPoint);
 
-		// Vecs
-		template<typename T> void rotateAroundCenter(Vec2<T>& mPoint, const Vec2<T>& mCenter, float mRadians)
+		// Vec
+		template<typename T> T getMagnitude(const Vec2<T>& mVec) { return sqrt(mVec.x * mVec.x + mVec.y * mVec.y); }
+		template<typename T> void rotateAroundCenter(Vec2<T>& mVec, const Vec2<T>& mCenter, float mRadians)
 		{
 			float s(sin(mRadians)), c(cos(mRadians));
-			mPoint -= mCenter;
-			Vec2<T> newPoint(mPoint.x * c - mPoint.y * s, mPoint.x * s + mPoint.y * c);
-			mPoint = newPoint + mCenter;
+			mVec -= mCenter;
+			Vec2<T> newPoint(mVec.x * c - mVec.y * s, mVec.x * s + mVec.y * c);
+			mVec = newPoint + mCenter;
 		}
-		template<typename T> T getMagnitude(const Vec2<T>& mVec) { return sqrt(mVec.x * mVec.x + mVec.y * mVec.y); }
-		template<typename T> Vec2<T> getNormalized(const Vec2<T>& mVec) { T m(getMagnitude(mVec)); return m == 0 ? mVec : Vec2<T>{mVec.x / m, mVec.y / m}; }
-		template<typename T> T getRadians(const Vec2<T>& mVec) { auto n(getNormalized(mVec)); return atan2(n.y, n.x); }
-		template<typename T> T getDegrees(const Vec2<T>& mVec) { return ssvu::toDegrees(getRadians(mVec)); }
+		template<typename T> void normalize(Vec2<T>& mVec) { T m(getMagnitude(mVec)); if(m != 0) mVec /= m; }
+		template<typename T> Vec2<T> getNormalized(Vec2<T> mVec) { normalize(mVec); return mVec; }
+		template<typename T> Vec2<T> getDirection(const Vec2<T>& mVec, const Vec2<T>& mTarget) { return getNormalized(mTarget - mVec); }
+		template<typename T> void moveTowards(Vec2<T>& mVec, const Vec2<T>& mTarget, T mMagnitude) { mVec += getDirection(mVec, mTarget) * mMagnitude; }
+		template<typename T> T getRadians(Vec2<T> mVec) { normalize(mVec); return atan2(mVec.y, mVec.x); }
+		template<typename T> T getDegrees(Vec2<T> mVec) { return ssvu::toDegrees(getRadians(mVec)); }
 		template<typename T> Vec2<T> getVecFromRadians(T mRadians, T mMagnitude) { return Vec2<T>(mMagnitude * cos(mRadians), mMagnitude * sin(mRadians)); }
 		template<typename T> Vec2<T> getVecFromDegrees(T mDegrees, T mMagnitude) { return getVecFromRadians(ssvu::toRadians(mDegrees), mMagnitude); }
 		template<typename T> Vec2<T> getOrbitFromRadians(const Vec2<T>& mVec, T mRadians, T mRadius) { return mVec + Vec2<T>(cos(mRadians), sin(mRadians)) * mRadius; }
 		template<typename T> Vec2<T> getOrbitFromDegrees(const Vec2<T>& mVec, T mDegrees, T mRadius) { return getOrbitFromRadians(mVec, ssvu::toRadians(mDegrees), mRadius); }
-		template<typename T> Vec2<T> getDirection(const Vec2<T>& mVec, const Vec2<T>& mTarget) { return getNormalized(mTarget - mVec); }
-		template<typename T> Vec2<T> getMovedTowards(const Vec2<T>& mVec, const Vec2<T>& mTarget, T mMagnitude) { return mVec + getDirection(mVec, mTarget) * mMagnitude; }
+		template<typename T> Vec2<T> getMovedTowards(Vec2<T> mVec, const Vec2<T>& mTarget, T mMagnitude) { moveTowards(mVec, mTarget, mMagnitude); return mVec; }
 		template<typename T> T getDotProduct(const Vec2<T>& mA, const Vec2<T>& mB) { return mA.x * mB.x + mA.y * mB.y; }
 		template<typename T> Vec2<T> getRotatedAroundCenter(Vec2<T> mPoint, const Vec2<T>& mCenter, float mRadians) { rotateAroundCenter(mPoint, mCenter, mRadians); return mPoint; }
 
