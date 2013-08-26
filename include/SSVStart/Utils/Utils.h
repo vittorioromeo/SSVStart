97fc56b0f02fb5a1db07d1ec8313bfce22f75b6f
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-26 16:13:52 +0200

    Added many new vec utils add2-3StateInput now allows user to specify triggertype

--- include/SSVStart/Utils/Utils.h
+++ include/SSVStart/Utils/Utils.h
@@ -5,6 +5,7 @@
 #ifndef SSVS_UTILS
 #define SSVS_UTILS
 
+#include <cassert>
 #include <math.h>
 #include <string>
 #include <sstream>
@@ -44,7 +45,7 @@ namespace ssvs
 			return result;
 		}
 
-		// Vec
+		// Vec utils
 		template<typename T> inline T getMagnitude(const Vec2<T>& mVec) { return sqrt(mVec.x * mVec.x + mVec.y * mVec.y); }
 		template<typename T> void rotateAroundCenter(Vec2<T>& mVec, const Vec2<T>& mCenter, float mRadians)
 		{
@@ -53,28 +54,56 @@ namespace ssvs
 			mVec = newPoint + mCenter;
 		}
 		template<typename T> inline void nullify(Vec2<T>& mVec) { mVec.x = mVec.y = 0; }
-		template<typename T> inline void normalize(Vec2<T>& mVec) { T m(getMagnitude(mVec)); if(m != 0) mVec /= m; }
+		template<typename T> inline void normalize(Vec2<T>& mVec) { const T& m(getMagnitude(mVec)); if(m != 0) mVec /= m; }
 		template<typename T> inline Vec2<T> getNormalized(Vec2<T> mVec) { normalize(mVec); return mVec; }
-		template<typename T> inline Vec2<T> getDirection(const Vec2<T>& mVec, const Vec2<T>& mTarget) { return getNormalized(mTarget - mVec); }
-		template<typename T> inline void moveTowards(Vec2<T>& mVec, const Vec2<T>& mTarget, T mMagnitude) { mVec += getDirection(mVec, mTarget) * mMagnitude; }
+
+		// Resize a vec maintaining direction
+		template<typename T> inline void resize(Vec2<T>& mVec, const T& mLength)	{ normalize(mVec); mVec *= mLength; }
+		template<typename T> inline void getResized(Vec2<T> mVec, const T& mLength) { normalize(mVec); return mVec * mLength; }
+
+		// Component-based vec clamping
+		template<typename T> inline void cClamp(Vec2<T>& mVec, const T& mMin, const T& mMax)		{ ssvu::clamp(mVec.x, mMin, mMax); ssvu::clamp(mVec.y, mMin, mMax); }
+		template<typename T> inline void cClampMin(Vec2<T>& mVec, const T& mMin)					{ ssvu::clampMin(mVec.x, mMin); ssvu::clampMin(mVec.y, mMin); }
+		template<typename T> inline void cClampMax(Vec2<T>& mVec, const T& mMax)					{ ssvu::clampMax(mVec.x, mMax); ssvu::clampMax(mVec.y, mMax); }
+		template<typename T> inline Vec2<T> getCClamped(Vec2<T> mVec, const T& mMin, const T& mMax)	{ cClamp(mVec, mMin, mMax); return mVec; }
+		template<typename T> inline Vec2<T> getCClampedMin(Vec2<T> mVec, const T& mMin)				{ cClampMin(mVec, mMin); return mVec; }
+		template<typename T> inline Vec2<T> getCClampedMax(Vec2<T> mVec, const T& mMax)				{ cClampMax(mVec, mMax); return mVec; }
+
+		// Magnitude-based vec clamping
+		template<typename T> inline void mClamp(Vec2<T>& mVec, const T& mMin, const T& mMax)		{ assert(mMin < mMax); const T& m(getMagnitude(mVec)); if(m < mMin) { resize(mVec, mMin); return; } if(m > mMax) { resize(mVec, mMax); return; } }
+		template<typename T> inline void mClampMin(Vec2<T>& mVec, const T& mMin)					{ if(getMagnitude(mVec) < mMin) resize(mVec, mMin); }
+		template<typename T> inline void mClampMax(Vec2<T>& mVec, const T& mMax)					{ if(getMagnitude(mVec) > mMax) resize(mVec, mMax); }
+		template<typename T> inline Vec2<T> getMClamped(Vec2<T> mVec, const T& mMin, const T& mMax)	{ mClamp(mVec, mMin, mMax); return mVec; }
+		template<typename T> inline Vec2<T> getMClampedMin(Vec2<T> mVec, const T& mMin)				{ mClampMin(mVec, mMin); return mVec; }
+		template<typename T> inline Vec2<T> getMClampedMax(Vec2<T> mVec, const T& mMax)				{ mClampMax(mVec, mMax); return mVec; }
+
+		// Get angle from vec direction
 		template<typename T> inline T getRadians(Vec2<T> mVec) { normalize(mVec); return atan2(mVec.y, mVec.x); }
 		template<typename T> inline T getDegrees(Vec2<T> mVec) { return ssvu::toDegrees(getRadians(mVec)); }
+
+		// Get unit vec from angle
 		template<typename T> inline Vec2<T> getVecFromRadians(T mRadians, T mMagnitude) { return Vec2<T>(mMagnitude * cos(mRadians), mMagnitude * sin(mRadians)); }
 		template<typename T> inline Vec2<T> getVecFromDegrees(T mDegrees, T mMagnitude) { return getVecFromRadians(ssvu::toRadians(mDegrees), mMagnitude); }
+
+		// Get direction between two vecs
+		template<typename T> inline Vec2<T> getDirection(const Vec2<T>& mVec, const Vec2<T>& mTarget) { return getNormalized(mTarget - mVec); }
+
+		// TODO: document
+		template<typename T> inline void moveTowards(Vec2<T>& mVec, const Vec2<T>& mTarget, T mMagnitude) { mVec += getDirection(mVec, mTarget) * mMagnitude; }
 		template<typename T> inline Vec2<T> getOrbitFromRadians(const Vec2<T>& mVec, T mRadians, T mRadius) { return mVec + Vec2<T>(cos(mRadians), sin(mRadians)) * mRadius; }
 		template<typename T> inline Vec2<T> getOrbitFromDegrees(const Vec2<T>& mVec, T mDegrees, T mRadius) { return getOrbitFromRadians(mVec, ssvu::toRadians(mDegrees), mRadius); }
 		template<typename T> inline Vec2<T> getMovedTowards(Vec2<T> mVec, const Vec2<T>& mTarget, T mMagnitude) { moveTowards(mVec, mTarget, mMagnitude); return mVec; }
 		template<typename T> inline T getDotProduct(const Vec2<T>& mA, const Vec2<T>& mB) { return mA.x * mB.x + mA.y * mB.y; }
 		template<typename T> inline Vec2<T> getRotatedAroundCenter(Vec2<T> mPoint, const Vec2<T>& mCenter, float mRadians) { rotateAroundCenter(mPoint, mCenter, mRadians); return mPoint; }
 
-		inline void add2StateInput(GameState& mGameState, const Input::Trigger& mTrigger, bool& mValue)
+		inline void add2StateInput(GameState& mGameState, const Input::Trigger& mTrigger, bool& mValue, Input::Trigger::Type mType = Input::Trigger::Type::Always)
 		{
-			mGameState.addInput(mTrigger, [&](float){ mValue = true; }, [&](float){ mValue = false; });
+			mGameState.addInput(mTrigger, [&](float){ mValue = true; }, [&](float){ mValue = false; }, mType);
 		}
-		inline void add3StateInput(GameState& mGameState, const Input::Trigger& mNegative, Input::Trigger mPositive, int& mValue)
+		inline void add3StateInput(GameState& mGameState, const Input::Trigger& mNegative, Input::Trigger mPositive, int& mValue, Input::Trigger::Type mType = Input::Trigger::Type::Always)
 		{
-			mGameState.addInput(mNegative, [&](float){ mValue = -1; },	[&](float){ if(mValue == -1) mValue = 0; });
-			mGameState.addInput(mPositive, [&](float){ mValue = 1; },	[&](float){ if(mValue == 1) mValue = 0; });
+			mGameState.addInput(mNegative, [&](float){ mValue = -1; },	[&](float){ if(mValue == -1) mValue = 0; }, mType);
+			mGameState.addInput(mPositive, [&](float){ mValue = 1; },	[&](float){ if(mValue == 1) mValue = 0; }, mType);
 		}
 	}
 }
