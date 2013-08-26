fd365c8ec7582133f57ab10a5311d85117bab459
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-26 15:34:58 +0200

    SoundPlayer overhaul (WIP)

--- include/SSVStart/SoundPlayer/SoundPlayer.h
+++ include/SSVStart/SoundPlayer/SoundPlayer.h
@@ -16,37 +16,43 @@ namespace ssvs
 			struct ManageableSound : public sf::Sound, public ssvu::MemoryManageable { using sf::Sound::Sound; };
 
 			int volume{100};
-			ssvu::MemoryManager<ManageableSound> memoryManager;
+			ssvu::MemoryManager<ManageableSound> sounds;
 
-			inline void refreshVolume() { for(auto& s : memoryManager) s->setVolume(volume); }
+			inline void refreshVolume() { for(auto& s : sounds) s->setVolume(volume); }
 
 		public:
 			enum class Mode{Overlap, Override, Abort};
 
-			inline void play(sf::SoundBuffer& mSoundBuffer, Mode mMode = Mode::Overlap)
+			inline ManageableSound& play(sf::SoundBuffer& mSoundBuffer, Mode mMode = Mode::Overlap, float mPitch = 1.f)
 			{
-				for(const auto& s : memoryManager) if(s->getStatus() == sf::Sound::Status::Stopped) memoryManager.del(*s);
-				memoryManager.refresh();
+				for(const auto& s : sounds) if(s->getStatus() == sf::Sound::Status::Stopped) sounds.del(*s);
+				sounds.refresh();
 
 				switch(mMode)
 				{
 					case Mode::Overlap: break;
-					case Mode::Override:
-						for(const auto& s : memoryManager) if(s->getBuffer() == &mSoundBuffer) s->stop();
-						break;
-					case Mode::Abort:
-						for(const auto& s : memoryManager) if(s->getBuffer() == &mSoundBuffer) return;
-						break;
+					case Mode::Override: stop(mSoundBuffer); break;
+					case Mode::Abort: if(isPlaying(mSoundBuffer)) return;
 				}
 
-				auto& sound(memoryManager.create(mSoundBuffer));
-				sound.setVolume(volume);
+				auto& sound(sounds.create(mSoundBuffer));
+				sound.setVolume(volume); sound.setPitch(mPitch);
 				sound.play();
+
+				return sound;
 			}
-			inline void stop() { for(auto& s : memoryManager) s->stop(); }
+			inline void stop() { for(auto& s : sounds) s->stop(); }
+			inline void stop(const sf::SoundBuffer& mSoundBuffer) { for(auto& s : sounds) if(s->getBuffer() == &mSoundBuffer) s->stop(); }
 
 			inline void setVolume(int mVolume)	{ volume = mVolume; refreshVolume(); }
 			inline int getVolume()				{ return volume; }
+
+			inline bool isPlaying(const sf::SoundBuffer& mSoundBuffer) const { return findFirst(mSoundBuffer) != nullptr; }
+			inline ManageableSound* findFirst(const sf::SoundBuffer& mSoundBuffer) const
+			{
+				for(const auto& s : sounds) if(s->getBuffer() == &mSoundBuffer) return s.get();
+				return nullptr;
+			}
 	};
 }
 
