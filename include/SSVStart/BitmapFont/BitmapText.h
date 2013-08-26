9466f594db6d88e45d107985debe8732d3c607e8
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-08-26 22:12:11 +0200

    Minor refactoring

--- include/SSVStart/BitmapFont/BitmapText.h
+++ include/SSVStart/BitmapFont/BitmapText.h
@@ -26,13 +26,11 @@ namespace ssvs
 
 			void refreshGeometry()
 			{
-				const auto& width(bitmapFont.getCellWidth());
-				const auto& height(bitmapFont.getCellHeight());
-				unsigned int iX{0}, iY{0};
+				unsigned int iX{0}, iY{0}, width{bitmapFont.getCellWidth()}, height{bitmapFont.getCellHeight()};;
 				float xMin{0}, xMax{0}, yMin{0}, yMax{0};
 
 				vertices.clear();
-				for(const auto& c : str)
+				for(char c : str)
 				{
 					switch(c)
 					{
@@ -44,15 +42,15 @@ namespace ssvs
 					const auto& rect(bitmapFont.getGlyphRect(c));
 					unsigned int spacing{tracking * iX};
 
-					const auto& gLeft(iX * width + spacing);			if(xMin > gLeft) xMin = gLeft;
-					const auto& gRight((iX + 1) * width + spacing);		if(xMax < gRight) xMax = gRight;
-					const auto& gTop(iY * height);						if(yMin > gTop) yMin = gTop;
-					const auto& gBottom((iY + 1) * height);				if(yMax < gBottom) yMax = gBottom;
+					float gLeft{iX * width + spacing};			if(xMin > gLeft) xMin = gLeft;
+					float gRight{(iX + 1) * width + spacing};	if(xMax < gRight) xMax = gRight;
+					float gTop{iY * height};					if(yMin > gTop) yMin = gTop;
+					float gBottom{(iY + 1) * height};			if(yMax < gBottom) yMax = gBottom;
 
-					vertices.append({Vec2f(gLeft,	gTop),		color,	Vec2f(rect.left,				rect.top)});
-					vertices.append({Vec2f(gRight,	gTop),		color,	Vec2f(rect.left + rect.width,	rect.top)});
-					vertices.append({Vec2f(gRight,	gBottom),	color,	Vec2f(rect.left + rect.width,	rect.top + rect.height)});
-					vertices.append({Vec2f(gLeft,	gBottom),	color,	Vec2f(rect.left,				rect.top + rect.height)});
+					vertices.append({{gLeft,	gTop},		color,	Vec2f(rect.left,				rect.top)});
+					vertices.append({{gRight,	gTop},		color,	Vec2f(rect.left + rect.width,	rect.top)});
+					vertices.append({{gRight,	gBottom},	color,	Vec2f(rect.left + rect.width,	rect.top + rect.height)});
+					vertices.append({{gLeft,	gBottom},	color,	Vec2f(rect.left,				rect.top + rect.height)});
 
 					++iX;
 				}
