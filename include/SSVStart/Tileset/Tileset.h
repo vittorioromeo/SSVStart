614fde21f1e17c9d528c3e66c8269aab060e88f7
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-09-23 16:11:59 +0200

    Changed tileset syntax - now uses () instead of []

--- include/SSVStart/Tileset/Tileset.h
+++ include/SSVStart/Tileset/Tileset.h
@@ -23,12 +23,15 @@ namespace ssvs
 			Tileset(const Vec2u& mTileSize) : tileSize{mTileSize} { }
 
 			inline void setTileSize(const Vec2u& mTileSize) noexcept				{ tileSize = mTileSize; }
-			inline void setLabel(const std::string& mLabel, const Vec2u& mIdx)	{ labels[mLabel] = mIdx; }
+			inline void setLabel(const std::string& mLabel, const Vec2u& mIdx)		{ labels[mLabel] = mIdx; }
 
-			inline const decltype(labels)& getLabels() const noexcept		{ return labels; }
-			inline const Vec2u& getTileSize() const noexcept				{ return tileSize; }
-			inline const Vec2u& getIndex(const std::string& mLabel) const	{ return labels.at(mLabel); }
-			inline sf::IntRect operator[](const Vec2u& mIdx) const			{ return sf::IntRect(mIdx.x * tileSize.x, mIdx.y * tileSize.y, tileSize.x, tileSize.y); }
+			inline const decltype(labels)& getLabels() const noexcept				{ return labels; }
+			inline const Vec2u& getTileSize() const noexcept						{ return tileSize; }
+			inline const Vec2u& getIndex(const std::string& mLabel) const			{ return labels.at(mLabel); }
+
+			inline sf::IntRect operator()(unsigned int mX, unsigned int mY) const	{ return sf::IntRect(mX * tileSize.x, mY * tileSize.y, tileSize.x, tileSize.y); }
+			inline sf::IntRect operator()(const Vec2u& mIdx) const					{ return (*this)(mIdx.x, mIdx.y); }
+			inline sf::IntRect operator()(const std::string& mLabel) const			{ return (*this)(getIndex(mLabel)); }
 	};
 }
 
