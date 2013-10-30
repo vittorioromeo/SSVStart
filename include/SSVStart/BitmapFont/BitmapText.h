5de9c376684c1fc1f60e5383e4b5ce32a2654ee8
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-10-30 23:03:35 +0100

    Adapted to newest libs Use mutable instead of ugly const_cast

--- include/SSVStart/BitmapFont/BitmapText.h
+++ include/SSVStart/BitmapFont/BitmapText.h
@@ -19,14 +19,16 @@ namespace ssvs
 			const BitmapFont& bitmapFont;
 			const sf::Texture& texture;
 			std::string str;
-			ssvs::VertexVector<sf::PrimitiveType::Quads> vertices;
-			sf::FloatRect bounds;
+			mutable ssvs::VertexVector<sf::PrimitiveType::Quads> vertices;
+			mutable sf::FloatRect bounds;
 			sf::Color color{sf::Color::White};
 			int tracking{0};
-			bool mustRefreshGeometry{true}, mustRefreshColor{true};
+			mutable bool mustRefreshGeometry{true}, mustRefreshColor{true};
 
-			inline void refreshGeometry()
+			inline void refreshGeometry() const
 			{
+				if(!mustRefreshGeometry) return;
+
 				unsigned int iX{0}, iY{0}, width{bitmapFont.getCellWidth()}, height{bitmapFont.getCellHeight()};
 				float xMin{0}, xMax{0}, yMin{0}, yMax{0};
 
@@ -59,19 +61,14 @@ namespace ssvs
 				bounds = {xMin, yMin, xMax - xMin, yMax - yMin};
 				mustRefreshGeometry = false;
 			}
-			inline void refreshColor() { for(auto& v : vertices) v.color = color; mustRefreshColor = false; }
-			inline void refresh() const
-			{
-				if(mustRefreshGeometry) const_cast<BitmapText*>(this)->refreshGeometry();
-				if(mustRefreshColor) const_cast<BitmapText*>(this)->refreshColor();
-			}
+			inline void refreshColor() const { if(!mustRefreshColor) return; for(auto& v : vertices) v.color = color; mustRefreshColor = false; }
 
 		public:
 			BitmapText(const BitmapFont& mBitmapFont, std::string mStr = "") : bitmapFont(mBitmapFont), texture(bitmapFont.getTexture()), str{std::move(mStr)} { }
 
 			inline void draw(sf::RenderTarget& mRenderTarget, sf::RenderStates mRenderStates) const override
 			{
-				refresh();
+				refreshGeometry(); refreshColor();
 				mRenderStates.transform *= getTransform();
 				mRenderStates.texture = &texture;
 				mRenderTarget.draw(vertices, mRenderStates);
@@ -85,7 +82,7 @@ namespace ssvs
 			inline const std::string& getString() const noexcept	{ return str; }
 			inline const sf::Color& getColor() const noexcept		{ return color; }
 			inline int getTracking() const noexcept					{ return tracking; }
-			inline const sf::FloatRect& getLocalBounds() const		{ refresh(); return bounds; }
+			inline const sf::FloatRect& getLocalBounds() const		{ refreshGeometry(); return bounds; }
 			inline sf::FloatRect getGlobalBounds() const			{ return getTransform().transformRect(getLocalBounds()); }
 	};
 }
