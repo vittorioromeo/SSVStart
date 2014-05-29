3cf927cc3e394bb4a47c429e0b62ea84b3b88f93
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-05-29 11:11:01 +0200

    Name refactoring / typedefs

--- include/SSVStart/Input/Manager.hpp
+++ include/SSVStart/Input/Manager.hpp
@@ -24,7 +24,7 @@ namespace ssvs
 			private:
 				KeyBitset processedKeys;
 				BtnBitset processedBtns;
-				ssvu::VectorUptr<Bind> binds;
+				ssvu::VecUptr<Bind> binds;
 
 			public:
 				inline void update(InputState& mInputState, FT mFT) { for(auto& b : binds) b->update(*this, mFT, mInputState); }
