ab862081e6d5a122485242b1e98178522cf479c1
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-10-11 16:37:37 +0200

    Adapted to newest libs

--- include/SSVStart/Json/UtilsJsonNew.hpp
+++ include/SSVStart/Json/UtilsJsonNew.hpp
@@ -19,79 +19,73 @@
 #include "SSVStart/Global/Typedefs.hpp"
 #include "SSVStart/Assets/AssetManager.hpp"
 
-// TODO: namespace macro?
-namespace ssvu
+SSVJ_CNV_NAMESPACE()
 {
-	namespace Json
+	template<typename T> SSVJ_CNV_SIMPLE(ssvs::Vec2<T>, mV, mX)	{ ssvj::convertArr(mV, mX.x, mX.y); }													SSVJ_CNV_END()
+	template<> SSVJ_CNV_SIMPLE(ssvs::BitmapFontData, mV, mX)	{ ssvj::convertArr(mV, mX.cellColumns, mX.cellWidth, mX.cellHeight, mX.cellStart); }	SSVJ_CNV_END()
+	template<> SSVJ_CNV_SIMPLE(sf::Color, mV, mX)				{ ssvj::convertArr(mV, mX.r, mX.g, mX.b, mX.a); }										SSVJ_CNV_END()
+	template<> SSVJ_CNV_SIMPLE(ssvs::Input::Trigger, mV, mX)	{ ssvj::convert(mV, mX.getCombos()); }													SSVJ_CNV_END()
+
+	template<> struct Cnv<ssvs::KKey>
 	{
-		namespace Internal
+		using T = ssvs::KKey;
+		inline static void fromVal(const Val& mV, T& mX)	{ mX = ssvs::getKKey(mV.as<std::string>()); }
+		inline static void toVal(Val& mV, const T& mX)		{ mV = ssvs::getKKeyName(mX); }
+	};
+	template<> struct Cnv<ssvs::MBtn>
+	{
+		using T = ssvs::MBtn;
+		inline static void fromVal(const Val& mV, T& mX)	{ mX = ssvs::getMBtn(mV.as<std::string>()); }
+		inline static void toVal(Val& mV, const T& mX)		{ mV = ssvs::getMBtnName(mX); }
+	};
+	template<> struct Cnv<ssvs::Input::Combo>
+	{
+		using T = ssvs::Input::Combo;
+		inline static void fromVal(const Val& mV, T& mX)
 		{
-			template<typename T> SSVJ_CNV_SIMPLE(ssvs::Vec2<T>, mV, mX)	{ ssvj::convertArr(mV, mX.x, mX.y); }													SSVJ_CNV_END();
-			template<> SSVJ_CNV_SIMPLE(ssvs::BitmapFontData, mV, mX)	{ ssvj::convertArr(mV, mX.cellColumns, mX.cellWidth, mX.cellHeight, mX.cellStart); }	SSVJ_CNV_END();
-			template<> SSVJ_CNV_SIMPLE(sf::Color, mV, mX)				{ ssvj::convertArr(mV, mX.r, mX.g, mX.b, mX.a); }										SSVJ_CNV_END();
-			template<> SSVJ_CNV_SIMPLE(ssvs::Input::Trigger, mV, mX)	{ ssvj::convert(mV, mX.getCombos()); }													SSVJ_CNV_END();
-
-			template<> struct Cnv<ssvs::KKey>
+			for(const auto& i : mV.forArr())
 			{
-				using T = ssvs::KKey;
-				inline static void fromVal(const Val& mV, T& mX)	{ mX = ssvs::getKKey(mV.as<std::string>()); }
-				inline static void toVal(Val& mV, const T& mX)		{ mV = ssvs::getKKeyName(mX); }
-			};
-			template<> struct Cnv<ssvs::MBtn>
-			{
-				using T = ssvs::MBtn;
-				inline static void fromVal(const Val& mV, T& mX)	{ mX = ssvs::getMBtn(mV.as<std::string>()); }
-				inline static void toVal(Val& mV, const T& mX)		{ mV = ssvs::getMBtnName(mX); }
-			};
-			template<> struct Cnv<ssvs::Input::Combo>
-			{
-				using T = ssvs::Input::Combo;
-				inline static void fromVal(const Val& mV, T& mX)
-				{
-					for(const auto& i : mV.forArr())
-					{
-						const auto& name(i.as<std::string>());
+				const auto& name(i.as<std::string>());
 
-						if(ssvs::isKKeyNameValid(name)) mX.addKey(i.as<ssvs::KKey>());
-						else if(ssvs::isMBtnNameValid(name)) mX.addBtn(i.as<ssvs::MBtn>());
-						else ssvu::lo("ssvs::getInputComboFromJSON") << "<" << i << "> is not a valid input name" << std::endl;
-					}
-				}
-				inline static void toVal(Val& mV, const T& mX)
-				{
-					mV = Arr{};
-					auto i(0u);
-					const auto& keys(mX.getKeys());
-					const auto& btns(mX.getBtns());
-					for(auto j(0u); j < ssvs::kKeyCount; ++j) if(ssvs::getKeyBit(keys, ssvs::KKey(j))) mV.as<Arr>().emplace_back(ssvs::KKey(j));
-					for(auto j(0u); j < ssvs::mBtnCount; ++j) if(ssvs::getBtnBit(btns, ssvs::MBtn(j))) mV.as<Arr>().emplace_back(ssvs::MBtn(j));
-				}
-			};
-
-			template<> struct Cnv<ssvs::Tileset>
-			{
-				using T = ssvs::Tileset;
-				inline static void fromVal(const Val& mV, T& mX)
-				{
-					const auto& labels(mV["labels"].as<Arr>());
-					for(auto iY(0u); iY < labels.size(); ++iY)
-						for(auto iX(0u); iX < labels[iY].as<Arr>().size(); ++iX)
-							mX.setLabel(labels[iY][iX].as<std::string>(), {iX, iY});
-
-					mX.setTileSize(mV["tileSize"].as<ssvs::Vec2u>());
-				}
-				inline static void toVal(Val& mV, const T& mX)
-				{
-					mV["tileSize"] = mX.getTileSize();
-
-					mV["labels"] = Arr{};
-					auto& labels(mV["labels"].as<Arr>());
-					for(const auto& l : mX.labels) labels[l.second.y][l.second.x] = l.first;
-				}
-			};
+				if(ssvs::isKKeyNameValid(name)) mX.addKey(i.as<ssvs::KKey>());
+				else if(ssvs::isMBtnNameValid(name)) mX.addBtn(i.as<ssvs::MBtn>());
+				else ssvu::lo("ssvs::getInputComboFromJSON") << "<" << i << "> is not a valid input name" << std::endl;
+			}
 		}
-	}
+		inline static void toVal(Val& mV, const T& mX)
+		{
+			mV = Arr{};
+			auto i(0u);
+			const auto& keys(mX.getKeys());
+			const auto& btns(mX.getBtns());
+			for(auto j(0u); j < ssvs::kKeyCount; ++j) if(ssvs::getKeyBit(keys, ssvs::KKey(j))) mV.as<Arr>().emplace_back(ssvs::KKey(j));
+			for(auto j(0u); j < ssvs::mBtnCount; ++j) if(ssvs::getBtnBit(btns, ssvs::MBtn(j))) mV.as<Arr>().emplace_back(ssvs::MBtn(j));
+		}
+	};
+
+	template<> struct Cnv<ssvs::Tileset>
+	{
+		using T = ssvs::Tileset;
+		inline static void fromVal(const Val& mV, T& mX)
+		{
+			const auto& labels(mV["labels"].as<Arr>());
+			for(auto iY(0u); iY < labels.size(); ++iY)
+				for(auto iX(0u); iX < labels[iY].as<Arr>().size(); ++iX)
+					mX.setLabel(labels[iY][iX].as<std::string>(), {iX, iY});
+
+			mX.setTileSize(mV["tileSize"].as<ssvs::Vec2u>());
+		}
+		inline static void toVal(Val& mV, const T& mX)
+		{
+			mV["tileSize"] = mX.getTileSize();
+
+			mV["labels"] = Arr{};
+			auto& labels(mV["labels"].as<Arr>());
+			for(const auto& l : mX.labels) labels[l.second.y][l.second.x] = l.first;
+		}
+	};
 }
+SSVJ_CNV_NAMESPACE_END()
 
 namespace ssvs
 {
