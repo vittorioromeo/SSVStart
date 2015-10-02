3899839310f15e9c97d69f0e91e47d4502cbe9b2
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-10-02 11:37:42 +0200

    Tests.cpp

---
+++ test/Json.cpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2013-2015 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
+#include "./utils/test_utils.hpp"
+#include <SSVStart/SSVStart.hpp>
+
+int main()
+{
+    using namespace std;
+    using namespace ssvu;
+    using namespace ssvj;
+    using namespace ssvs;
+
+#define EXECTEST                                                \
+    do                                                          \
+    {                                                           \
+        auto s(Val{original}.getWriteToStr());                  \
+        TEST_ASSERT_NS_OP(fromStr(s).as<Type>(), ==, original); \
+    } while(false)
+
+#define MAKETEST(T, ...)            \
+    do                              \
+    {                               \
+        using Type = T;             \
+        Type original(__VA_ARGS__); \
+        EXECTEST;                   \
+    } while(false)
+
+    MAKETEST(ssvs::Vec2f, 52.f, 33.f);
+    MAKETEST(ssvs::Vec2i, 52, -33);
+    MAKETEST(ssvs::Vec2u, 52, 33);
+
+    MAKETEST(sf::Color, 52, 33, 22);
+    MAKETEST(sf::Color, 0, 0, 0);
+    MAKETEST(sf::Color, 255, 255, 255);
+
+    MAKETEST(ssvs::KKey, ssvs::KKey::Up);
+    MAKETEST(ssvs::KKey, ssvs::KKey::A);
+    MAKETEST(ssvs::KKey, ssvs::KKey::B);
+    MAKETEST(ssvs::KKey, ssvs::KKey::Num0);
+    MAKETEST(ssvs::KKey, ssvs::KKey::F1);
+
+    MAKETEST(ssvs::MBtn, ssvs::MBtn::Left);
+    MAKETEST(ssvs::MBtn, ssvs::MBtn::XButton1);
+
+    MAKETEST(ssvs::Input::Combo, {ssvs::KKey::Up}, {ssvs::MBtn::Left});
+    MAKETEST(ssvs::Input::Trigger,
+             {{{ssvs::KKey::Up}, {ssvs::MBtn::Left}}, {ssvs::KKey::F1}});
+
+#undef EXECTEST
+#undef MAKETEST
+}
---
+++ test/UtilsInput.cpp
@@ -0,0 +1,45 @@
+// Copyright (c) 2013-2015 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
+#include "./utils/test_utils.hpp"
+#include <SSVStart/SSVStart.hpp>
+
+int main()
+{
+    TEST_ASSERT_NS(ssvs::isKKeyNameValid("kUp"));
+    TEST_ASSERT_NS(ssvs::isKKeyNameValid("kA"));
+    TEST_ASSERT_NS(ssvs::isKKeyNameValid("kB"));
+    TEST_ASSERT_NS(ssvs::isKKeyNameValid("kNum0"));
+    TEST_ASSERT_NS(ssvs::isKKeyNameValid("kF1"));
+    TEST_ASSERT_NS(!ssvs::isKKeyNameValid(""));
+    TEST_ASSERT_NS(!ssvs::isKKeyNameValid("Up"));
+    TEST_ASSERT_NS(!ssvs::isKKeyNameValid("kup"));
+    TEST_ASSERT_NS(!ssvs::isKKeyNameValid("F1"));
+    TEST_ASSERT_NS(!ssvs::isKKeyNameValid("kf1"));
+
+    TEST_ASSERT_NS(ssvs::isMBtnNameValid("bLeft"));
+    TEST_ASSERT_NS(ssvs::isMBtnNameValid("bXButton1"));
+    TEST_ASSERT_NS(!ssvs::isMBtnNameValid(""));
+    TEST_ASSERT_NS(!ssvs::isMBtnNameValid("Left"));
+    TEST_ASSERT_NS(!ssvs::isMBtnNameValid("XButton1"));
+    TEST_ASSERT_NS(!ssvs::isMBtnNameValid("bleft"));
+
+    TEST_ASSERT_NS(ssvs::getKKey("kUp") == ssvs::KKey::Up);
+    TEST_ASSERT_NS(ssvs::getKKey("kA") == ssvs::KKey::A);
+    TEST_ASSERT_NS(ssvs::getKKey("kB") == ssvs::KKey::B);
+    TEST_ASSERT_NS(ssvs::getKKey("kNum0") == ssvs::KKey::Num0);
+    TEST_ASSERT_NS(ssvs::getKKey("kF1") == ssvs::KKey::F1);
+
+    TEST_ASSERT_NS(ssvs::getMBtn("bLeft") == ssvs::MBtn::Left);
+    TEST_ASSERT_NS(ssvs::getMBtn("bXButton1") == ssvs::MBtn::XButton1);
+
+    TEST_ASSERT_NS(ssvs::getKKeyName(ssvs::KKey::Up) == "kUp");
+    TEST_ASSERT_NS(ssvs::getKKeyName(ssvs::KKey::A) == "kA");
+    TEST_ASSERT_NS(ssvs::getKKeyName(ssvs::KKey::B) == "kB");
+    TEST_ASSERT_NS(ssvs::getKKeyName(ssvs::KKey::Num0) == "kNum0");
+    TEST_ASSERT_NS(ssvs::getKKeyName(ssvs::KKey::F1) == "kF1");
+
+    TEST_ASSERT_NS(ssvs::getMBtnName(ssvs::MBtn::Left) == "bLeft");
+    TEST_ASSERT_NS(ssvs::getMBtnName(ssvs::MBtn::XButton1) == "bXButton1");
+}
